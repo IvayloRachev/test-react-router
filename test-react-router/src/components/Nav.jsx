23caa6032337c3986ccde85663726f13b387e377
IvayloRachev <rachev.ivaylo@yahoo.fr>
2023-02-28 11:21:36 +0100

    Summary

---
+++ test-react-router/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+    return ( 
+        <nav>
+            <h2 className='logo'>Home</h2>
+            <div className="links">
+                <div>A propos</div>
+                <div>Home</div>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Nav;
