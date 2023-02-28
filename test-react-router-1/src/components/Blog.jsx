ca7b20753962bbcca93b4766e8dd3464bc2d605b
IvayloRachev <rachev.ivaylo@yahoo.fr>
2023-02-28 16:18:29 +0100

    Summary

--- test-react-router-1/src/components/Blog.jsx
+++ test-react-router-1/src/components/Blog.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import '../lib';
+import { Link } from 'react-router-dom';
 
 const Blog = () => {
+        useEffect(() => {
+            getAllPosts();
+        }, [])
+        const [posts, setPosts] = useState([])
+        const getAllPosts = async () => {
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+            const allPosts = await response.json()
+            console.log(allPosts);
+            setPosts(allPosts);
+        }
     return ( 
-        <h1>Blog</h1>
-     );
+        <div>
+            <h1>Blog</h1>
+            {posts.take().map(post => <h4><Link key={post.id}>{post.title}</Link></h4>)}
+        </div>
+    );
 }
  
 export default Blog;
